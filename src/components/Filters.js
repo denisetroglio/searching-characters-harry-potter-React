acc400b73db591f4e70aed0c808a4c3b07efbfbd
Denise Troglio <overbeck.denise@gmail.com>
2022-01-27 13:12:33 +0100

    add webpage npm run build

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,13 +4,13 @@ import FilterHouse from "./FilterHouse";
 const Filters = (props) => {
   return (
     <form className='new_character'>
-      <h2 className='character_title'>Busque un personaje de la serie:</h2>
+      <h2 className='character_title'><i className="fas fa-quidditch"></i>Busque un personaje de la serie:</h2>
       <FilterName
         handleFilter={props.handleFilter}
         FilterName={props.FilterName}
       />
 
-      <h2 className='homeTitle'>Selecciona la casa:</h2>
+      <h2 className='homeTitle'><i className="fas fa-quidditch"></i>Selecciona la casa:</h2>
       <FilterHouse FilterHouse={props.FilterHouse} handleFilter={props.handleFilter}/>
     </form>
   );
